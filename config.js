fe25c1cb900a6c39ba77238008e8f0d1464abfa3
SJhonKw <1021836554@qq.com>
2023-05-06 15:09:41 +0800

    init

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    testappid: 'wxba70d72fd3720003',
+    testapp_secret: 'bbcddd04bf33aaf7506448df97820f15',
+    appid: 'wxfacf647e7610f35e',
+    app_secret: '92f6285af38b31bf9a280a3710966b39',
+    token: 'WechatOfficialDevelopment8iKEVIN',
+    EncodingAESKey: 'lPdfQCDoEIfqJr4bRtmGATBlkQUc6LIBj9m22tsd86w',
+    url: 'http://sjhonkw.cn/'
+}
+
+module.exports = config;
